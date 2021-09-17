ad4770149b216ce3d77ee0c4e8faf6b0090def5a
Javkhlan K <40118124+JavkhlanK@users.noreply.github.com>
2021-09-17 11:58:00 +0200

    Add hard game mode

--- TicTacToe/TicTacToe.c
+++ TicTacToe/TicTacToe.c
@@ -22,24 +22,37 @@
 
 int field[3][3];
 
-int parseCommandlineArguments();
+
+/* Core methods */
 
 void fillBoard();
 
-void processNewMove();
-
-void computerMoveModeEasy();
-
-void computerMoveEasyPick(int unavailableNumbers[], int unavailableNumbersSize, int pick);
+int parseCommandlineArguments();
 
 void printPlayground();
 
-bool isGameOverAndPrintMessages();
+void processNewMove();
+
+
+/* Helper methods */
 
 int currentWinner();
 
 bool isBoardFull();
 
+bool isGameOverAndPrintMessages();
+
+
+/* Various computer "AI" methods */
+
+void computerMoveEasyPick(int unavailableNumbers[], int unavailableNumbersSize, int pick);
+
+void computerMoveModeEasy();
+
+void computerMoveModeHard();
+
+
+/* Core methods */
 
 int main(int _argc, char *_argv[])
 {
@@ -127,6 +140,17 @@ int main(int _argc, char *_argv[])
 	return EXIT_SUCCESS;
 }
 
+void fillBoard()
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			field[i][j] = VALUE_FREE;
+		}
+	}
+}
+
 int parseCommandlineArguments(int _argc, char *_argv[])
 {
 	if (_argc == 1)
@@ -151,13 +175,51 @@ int parseCommandlineArguments(int _argc, char *_argv[])
 	return COMMAND_PROCESSED_SUCCESS;
 }
 
-void fillBoard()
+void printPlayground()
 {
+	printf(OUTPUT_CURRENT_BOARD, OUTPUT_CURRENT_BOARD_MARGIN_CHARS, OUTPUT_CURRENT_BOARD_H_OUTLINES);
 	for (int i = 0; i < 3; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < 14; j++)
 		{
-			field[i][j] = VALUE_FREE;
+			if (j == 2)
+			{
+				printf("%c", field[i][0] == VALUE_COMPUTER ? 'O' : field[i][0] == VALUE_USER ? 'X' : ' ');
+			}
+			else if (j == 6)
+			{
+				printf("%c", field[i][1] == VALUE_COMPUTER ? 'O' : field[i][1] == VALUE_USER ? 'X' : ' ');
+			}
+			else if (j == 10)
+			{
+				printf("%c", field[i][2] == VALUE_COMPUTER ? 'O' : field[i][2] == VALUE_USER ? 'X' : ' ');
+			}
+			else if (j == 13)
+			{
+				printf("\n");
+			}
+			else
+			{
+				printf((j % 2 == 0) ? "|" : " ");
+			}
+		}
+	}
+	printf("%s", OUTPUT_CURRENT_BOARD_H_OUTLINES);
+	printf("%s", OUTPUT_CURRENT_BOARD_MARGIN_CHARS);
+
+	if (DUMP_FIELD)
+	{
+		printf("Dump of field:\n[");
+		for (int i = 0; i < 9; i++)
+		{
+			if (i == 8)
+			{
+				printf("%d (i = %d)]\n", field[2][2], i);
+			}
+			else
+			{
+				printf("%d (i = %d), ", field[i < 3 ? 0 : i < 6 ? 1 : 2][i % 3], i);
+			}
 		}
 	}
 }
@@ -246,7 +308,7 @@ void processNewMove()
 	}
 	else if (DIFFICULTY == DIFFICULTY_COMPUTER_HARD)
 	{
-		//TODO: Add hard game mode
+		computerMoveModeHard();
 	}
 	else if (DIFFICULTY == DIFFICULTY_PLAYER_VS_PLAYER)
 	{
@@ -257,6 +319,117 @@ void processNewMove()
 		printf("%s", OUTPUT_ERROR_GENERIC_HOW_DID_WE_GET_HERE);
 	}
 	
+	if (isGameOverAndPrintMessages())
+	{
+		return;
+	}
+
+	printf("%s", OUTPUT_COMPUTER_MOVE_DONE);
+	processNewMove();
+}
+
+/* Helper methods */
+
+int currentWinner()
+{
+	int winner = WIN_NO_WIN_YET;
+	for (int i = 0; i < 2; i++)
+	{
+		int currentEntity = ((i == 0) ? VALUE_COMPUTER : VALUE_USER);
+		if (DEBUG) printf("Field check for %s!\n", currentEntity == VALUE_COMPUTER ? "computer" : currentEntity == VALUE_USER ? "player" : "unknown");
+
+		if (((field[0][0] == currentEntity) && (field[0][1] == currentEntity) && (field[0][2] == currentEntity)) || ((field[1][0] == currentEntity) && (field[1][1] == currentEntity) && (field[1][2] == currentEntity)) || ((field[2][0] == currentEntity) && (field[2][1] == currentEntity) && (field[2][2] == currentEntity)))
+		{
+			/*
+				A A A		_ _ _		_ _ _
+				_ _ _	or	A A A	or	_ _ _
+				_ _ _		_ _ _		A A A
+			*/
+
+			winner = currentEntity;
+			break;
+		}
+		else if ((field[0][0] == currentEntity && field[1][0] == currentEntity && field[2][0] == currentEntity) || (field[0][1] == currentEntity && field[1][1] == currentEntity && field[2][1] == currentEntity) || (field[0][2] == currentEntity && field[1][2] == currentEntity && field[2][2] == currentEntity))
+		{
+			/*
+				A _ _		_ A _		_ _ A
+				A _ _	or	_ A _	or	_ _ A
+				A _ _		_ A _		_ _ A
+			*/
+
+			winner = currentEntity;
+			break;
+		}
+		else if ((field[0][0] == currentEntity && field[1][1] == currentEntity && field[2][2] == currentEntity) || (field[0][2] == currentEntity && field[1][1] == currentEntity && field[2][0] == currentEntity))
+		{
+			/*
+				A _ _		_ _ A
+				_ A _	or	_ A _
+				_ _ A		A _ _
+			*/
+
+			winner = currentEntity;
+			break;
+		}
+	}
+	if (DEBUG) printf("Winner: %d\n", winner);
+
+	if (winner == WIN_NO_WIN_YET && isBoardFull())
+	{
+		winner = WIN_TIE;
+	}
+
+	return winner;
+}
+
+bool isBoardFull()
+{
+	bool notFull = true;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			if (!notFull)
+			{
+				break;
+			}
+
+			notFull = field[i][j] != VALUE_FREE;
+		}
+
+		if (!notFull)
+		{
+			break;
+		}
+	}
+
+	return notFull;
+}
+
+bool isGameOverAndPrintMessages()
+{
+	int winner = currentWinner();
+
+	if (winner == WIN_NO_WIN_YET)
+	{
+		return false;
+	}
+	printPlayground();
+
+	if (winner == WIN_COMPUTER_WON)
+	{
+		printf("%s", OUTPUT_GAME_OVER_LOSS);
+	}
+	else if (winner == WIN_USER_WON)
+	{
+		printf("%s", OUTPUT_GAME_OVER_VICTORY);
+	}
+	else if (winner == WIN_TIE)
+	{
+		printf("%s", OUTPUT_GAME_OVER_TIE);
+	}
+
+	return true;
 }
 
 void computerMoveModeEasy()
@@ -304,161 +477,147 @@ void computerMoveEasyPick(int unavailableNumbers[], int unavailableNumbersSize, 
 	{
 		field[0][pick] = VALUE_COMPUTER;
 	}
+}
 
-	if (isGameOverAndPrintMessages())
+void computerMoveModeHard()
+{
+	if (field[1][1] == VALUE_FREE)
 	{
+		field[1][1] = VALUE_COMPUTER;
 		return;
 	}
 
-	printf("%s", OUTPUT_COMPUTER_MOVE_DONE);
-	processNewMove();
-}
+	/* At first: check if anything should be hindered/done to win,
+	   like the computer/user being 1 move away from winning */
 
-void printPlayground()
-{
-	printf(OUTPUT_CURRENT_BOARD, OUTPUT_CURRENT_BOARD_MARGIN_CHARS, OUTPUT_CURRENT_BOARD_H_OUTLINES);
-	for (int i = 0; i < 3; i++)
+	for (int entityType = 0; entityType < 2; entityType++)
 	{
-		for (int j = 0; j < 14; j++)
+		int entity = entityType == 0 ? VALUE_COMPUTER : VALUE_USER;
+
+		/*
+			O _ _		_ O _		_ _ O
+			O _ _	or	_ O _	or	_ _ O
+			O _ _		_ O _		_ _ O
+		*/
+
+		for (int column = 0; column < 3; column++)
 		{
-			if (j == 2)
+			if (field[column][0] == entity && field[column][1] == entity && field[column][2] == VALUE_FREE)
 			{
-				printf("%c", field[i][0] == VALUE_COMPUTER ? 'O' : field[i][0] == VALUE_USER ? 'X' : ' ');
+				field[column][2] = VALUE_COMPUTER;
+				return;
 			}
-			else if (j == 6)
+			else if (field[column][1] == entity && field[column][2] == entity && field[column][0] == VALUE_FREE)
 			{
-				printf("%c", field[i][1] == VALUE_COMPUTER ? 'O' : field[i][1] == VALUE_USER ? 'X' : ' ');
+				field[column][0] = VALUE_COMPUTER;
+				return;
 			}
-			else if (j == 10)
+			else if (field[column][0] == entity && field[column][2] == entity && field[column][1] == VALUE_FREE)
 			{
-				printf("%c", field[i][2] == VALUE_COMPUTER ? 'O' : field[i][2] == VALUE_USER ? 'X' : ' ');
-			}
-			else if (j == 13)
-			{
-				printf("\n");
-			}
-			else
-			{
-				printf((j % 2 == 0) ? "|" : " ");
+				field[column][1] = VALUE_COMPUTER;
+				return;
 			}
 		}
-	}
-	printf("%s", OUTPUT_CURRENT_BOARD_H_OUTLINES);
-	printf("%s", OUTPUT_CURRENT_BOARD_MARGIN_CHARS);
 
-	if (DUMP_FIELD)
-	{
-		printf("Dump of field:\n[");
-		for (int i = 0; i < 9; i++)
+
+		/*
+			O O O		_ _ _		_ _ _
+			_ _ _	or	O O O	or	_ _ _
+			_ _ _		_ _ _		O O O
+		*/
+
+		for (int row = 0; row < 3; row++)
 		{
-			if (i == 8)
+			int entity = entityType == 0 ? VALUE_COMPUTER : VALUE_USER;
+			if (field[0][row] == entity && field[1][entity] == entity && field[2][row] == VALUE_FREE)
 			{
-				printf("%d (i = %d)]\n", field[2][2], i);
+				field[2][row] = VALUE_COMPUTER;
+				return;
 			}
-			else
+			else if (field[1][row] == entity && field[2][row] == entity && field[0][row] == VALUE_FREE)
 			{
-				printf("%d (i = %d), ", field[i < 3 ? 0 : i < 6 ? 1 : 2][i % 3], i);
+				field[0][row] = VALUE_COMPUTER;
+				return;
 			}
-		}
-	}
-}
-
-bool isGameOverAndPrintMessages()
-{
-	int winner = currentWinner();
-
-	if (winner == WIN_NO_WIN_YET)
-	{
-		return false;
-	}
-	printPlayground();
-
-	if (winner == WIN_COMPUTER_WON)
-	{
-		printf("%s", OUTPUT_GAME_OVER_LOSS);
-	}
-	else if (winner == WIN_USER_WON)
-	{
-		printf("%s", OUTPUT_GAME_OVER_VICTORY);
-	}
-	else if (winner == WIN_TIE)
-	{
-		printf("%s", OUTPUT_GAME_OVER_TIE);
-	}
-
-	return true;
-}
-
-int currentWinner()
-{
-	int winner = WIN_NO_WIN_YET;
-	for (int i = 0; i < 2; i++)
-	{
-		int currentEntity = ((i == 0) ? VALUE_COMPUTER : VALUE_USER);
-		if (DEBUG) printf("Field check for %s!\n", currentEntity == VALUE_COMPUTER ? "computer" : currentEntity == VALUE_USER ? "player" : "unknown");
-
-		if (((field[0][0] == currentEntity) && (field[0][1] == currentEntity) && (field[0][2] == currentEntity)) || ((field[1][0] == currentEntity) && (field[1][1] == currentEntity) && (field[1][2] == currentEntity)) || ((field[2][0] == currentEntity) && (field[2][1] == currentEntity) && (field[2][2] == currentEntity)))
-		{
-			/*
-			    X X X        _ _ _        _ _ _
-			    _ _ _   or   X X X   or   _ _ _
-			    _ _ _        _ _ _        X X X
-			*/
-			winner = currentEntity;
-			break;
-		}
-		else if ((field[0][0] == currentEntity && field[1][0] == currentEntity && field[2][0] == currentEntity) || (field[0][1] == currentEntity && field[1][1] == currentEntity && field[2][1] == currentEntity) || (field[0][2] == currentEntity && field[1][2] == currentEntity && field[2][2] == currentEntity))
-		{
-			/*
-			    X _ _        _ X _        _ _ X
-			    X _ _   or   _ X _   or   _ _ X
-			    X _ _        _ X _        _ _ X
-			*/
-			winner = currentEntity;
-			break;
-		}
-		else if ((field[0][0] == currentEntity && field[1][1] == currentEntity && field[2][2] == currentEntity) || (field[0][2] == currentEntity && field[1][1] == currentEntity && field[2][0] == currentEntity))
-		{
-			/*
-				X _ _        _ _ X
-				_ X _   or   _ X _
-				_ _ X        X _ _
-
-			*/
-			winner = currentEntity;
-			break;
-		}
-	}
-	if (DEBUG) printf("Winner: %d\n", winner);
-
-	if (winner == WIN_NO_WIN_YET && isBoardFull())
-	{
-		winner = WIN_TIE;
-	}
-
-	return winner;
-}
-
-bool isBoardFull()
-{
-	bool notFull = true;
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			if (!notFull)
+			else if (field[0][row] == entity && field[2][row] == entity && field[1][row] == VALUE_FREE)
 			{
-				break;
+				field[1][row] = VALUE_COMPUTER;
+				return;
 			}
-
-			notFull = field[i][j] != VALUE_FREE;
 		}
 
-		if (!notFull)
+
+		/* 
+			A _ _		X _ _		A _ _
+			_ A _	or	_ A _	or	_ X _
+			_ _ X		_ _ A		_ _ A
+		*/
+
+		if (field[0][0] == entity && field[1][1] == entity && field[2][2] == VALUE_FREE)
 		{
-			break;
+			field[2][2] = VALUE_COMPUTER;
+			return;
+		}
+		else if (field[1][1] == entity && field[2][2] == entity && field[0][0] == VALUE_FREE)
+		{
+			field[0][0] = VALUE_COMPUTER;
+			return;
+		}
+		else if (field[0][0] == entity && field[2][2] == entity && field[1][1] == VALUE_FREE)
+		{
+			field[1][1] = VALUE_COMPUTER;
+			return;
+		}
+
+
+		/*
+			_ _ A		_ _ X		_ _ A
+			_ A _	or	_ A _	or	_ X _
+			X _ _		A _ _		A _ _
+		*/
+
+		if (field[0][2] == entity && field[1][1] == entity && field[2][0] == VALUE_FREE)
+		{
+			field[2][0] = VALUE_COMPUTER;
+			return;
+		}
+		else if (field[1][1] == entity && field[2][0] == entity && field[0][2] == VALUE_FREE)
+		{
+			field[0][2] = VALUE_COMPUTER;
+			return;
+		}
+		else if (field[0][2] == entity && field[2][0] == entity && field[1][1] == VALUE_FREE)
+		{
+			field[1][1] = VALUE_COMPUTER;
+			return;
 		}
 	}
 
-	return notFull;
+
+	/*
+		X C _		_ C X		_ _ _		_ _ _
+		C _ _	or	_ _ C	or	_ _ C	or	C _ _
+		_ _ _		_ _ _		_ C X		X C _
+	*/
+
+	if (field[0][1] == VALUE_COMPUTER && field[1][0] == VALUE_COMPUTER && field[0][0] == VALUE_FREE)
+	{
+		field[0][0] = VALUE_COMPUTER;
+		return;
+	}
+	if (field[0][1] == VALUE_COMPUTER && field[1][2] == VALUE_COMPUTER && field[0][2] == VALUE_FREE)
+	{
+		field[0][2] = VALUE_COMPUTER;
+		return;
+	}
+	if (field[1][0] == VALUE_COMPUTER && field[2][1] == VALUE_COMPUTER && field[2][0] == VALUE_FREE)
+	{
+		field[2][0] = VALUE_COMPUTER;
+		return;
+	}
+	if (field[2][1] == VALUE_COMPUTER && field[1][2] == VALUE_COMPUTER && field[2][2] == VALUE_FREE)
+	{
+		field[2][2] = VALUE_COMPUTER;
+		return;
+	}
 }
